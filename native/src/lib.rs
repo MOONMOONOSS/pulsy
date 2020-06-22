72d4caefbc6f5adcf4ac28f1796b0e89595559fc
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-06-21 20:42:38 -0400

    It works!

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,19 +1,32 @@
 #[macro_use]
 extern crate serde;
 
+use futures::TryStreamExt;
 use neon::prelude::*;
 use neon::{declare_types, register_module};
+use neon::types::{JsFunction, JsUndefined, JsValue};
 use pulsar::{
-  Consumer as Pconsumer, DeserializeMessage, Error as PulsarError,
-  Pulsar, TokioExecutor, message::Payload,
+  DeserializeMessage, Error as PulsarError,
+  Pulsar, TokioExecutor,message::Payload,
   message::proto::command_subscribe::SubType,
+};
+use std::{
+  sync::{
+    Arc, Mutex,
+    mpsc::{self, RecvTimeoutError, TryRecvError},
+  },
+  thread, time::Duration,
 };
 use tokio::runtime::Runtime;
 
-#[derive(Serialize, Deserialize)]
+pub enum Event {
+  Tick { msg: String },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct PulsarMessage {
   #[serde(rename(serialize = "type", deserialize = "subject"))]
-  pub subject: String,
+  pub subject: Option<String>,
   #[serde(skip_serializing_if = "Option::is_none")]
   pub uuid: Option<String>,
   #[serde(skip_serializing_if = "Option::is_none")]
@@ -30,10 +43,111 @@ impl DeserializeMessage for PulsarMessage {
   }
 }
 
+fn event_thread(
+  address: Handle<JsString>,
+  uuid: Handle<JsString>,
+  shutdown_rx: mpsc::Receiver<()>
+) -> mpsc::Receiver<Event> {
+  let (tx, events_rx) = mpsc::channel();
+  let address = address.value();
+  let uuid = uuid.value();
+
+  thread::spawn(move || {
+    let mut runtime = Runtime::new().unwrap();
+
+    let pulsar: Result<
+      Pulsar<TokioExecutor>,
+      PulsarError,
+    > = runtime.block_on(Pulsar::builder(address)
+      .build()
+    );
+
+    // Yes I really have to do this.
+    let pulsar = pulsar.unwrap();
+
+    // Create the consumer
+    let mut consumer = runtime.block_on(pulsar
+      .consumer()
+      .with_topic("non-persistent://moonmoon/lottery/drawings")
+      .with_consumer_name(&uuid)
+      .with_subscription_type(SubType::Exclusive)
+      .with_subscription(&uuid)
+      .build()
+    ).unwrap();
+
+    loop {
+      match shutdown_rx.try_recv() {
+        Ok(_) | Err(TryRecvError::Disconnected) => {
+          break;
+        },
+        Err(TryRecvError::Empty) => {},
+      }
+
+      match runtime.block_on(consumer.try_next()).unwrap() {
+        Some::<pulsar::consumer::Message<PulsarMessage>>(msg) => {
+          let data = msg.deserialize().unwrap();
+
+          tx.send(Event::Tick { msg: serde_json::to_string(&data).unwrap() }).expect("Send failed");
+        },
+        None => {},
+      }
+    }
+  });
+
+  events_rx
+}
+
+pub struct EventEmitterTask(Arc<Mutex<mpsc::Receiver<Event>>>);
+
+impl Task for EventEmitterTask {
+  type Output = Option<Event>;
+  type Error = String;
+  type JsEvent = JsValue;
+
+  fn perform(&self) -> Result<Self::Output, Self::Error> {
+    let rx = self
+      .0
+      .lock()
+      .map_err(|_| "Could not obtain lock on receiver".to_string())?;
+
+    match rx.recv_timeout(Duration::from_millis(100)) {
+      Ok(event) => Ok(Some(event)),
+      Err(RecvTimeoutError::Timeout) => Ok(None),
+      Err(RecvTimeoutError::Disconnected) => Err("Failed to receive event".to_string()),
+    }
+  }
+
+  fn complete(
+    self,
+    mut cx: TaskContext,
+    event: Result<Self::Output, Self::Error>,
+  ) -> JsResult<Self::JsEvent> {
+    let event = event.or_else(|err| cx.throw_error(&err.to_string()))?;
+
+    let event = match event {
+      Some(event) => event,
+      None => return Ok(JsUndefined::new().upcast()),
+    };
+
+    let o = cx.empty_object();
+
+    match event {
+      Event::Tick { msg } => {
+        let event_name = cx.string("tick");
+        let event_msg = cx.string(msg);
+
+        o.set(&mut cx, "event", event_name)?;
+        o.set(&mut cx, "payload", event_msg)?;
+      }
+    }
+
+    Ok(o.upcast())
+  }
+}
+
 pub struct PulsarConsumer {
-  runtime: Runtime,
-  pulsar: Pulsar<TokioExecutor>,
-  consumer: Pconsumer<PulsarMessage>,
+  events: Arc<Mutex<mpsc::Receiver<Event>>>,
+  shutdown: mpsc::Sender<()>,
 }
 
 declare_types! {
@@ -42,40 +156,42 @@ declare_types! {
       // Get strings from JS Land
       let address: Handle<JsString> = cx.argument::<JsString>(0)?;
       let uuid: Handle<JsString> = cx.argument::<JsString>(1)?;
+      let (shutdown, shutdown_rx) = mpsc::channel();
 
-      println!("{}", address.value());
-      println!("{}", uuid.value());
-
-      // Spawn the Tokio runtime
-      let mut runtime = Runtime::new().unwrap();
-
-      let pulsar: Result<
-        Pulsar<TokioExecutor>,
-        PulsarError,
-      > = runtime.block_on(Pulsar::builder(address.value()).build());
-
-      // Yes I really have to do this.
-      let pulsar = pulsar.unwrap();
-
-      // Create the consumer
-      let mut consumer = runtime.block_on(pulsar
-        .consumer()
-        .with_topic("non-persistent://moonmoon/lottery/drawings")
-        .with_consumer_name(uuid.value())
-        .with_subscription_type(SubType::Exclusive)
-        .with_subscription(uuid.value())
-        .build()
-      ).unwrap();
+      // Start worker thread
+      let rx = event_thread(address, uuid, shutdown_rx);
 
       Ok(PulsarConsumer {
-        runtime,
-        pulsar,
-        consumer,
+        events: Arc::new(Mutex::new(rx)),
+        shutdown,
       })
+    }
+
+    method poll(mut cx) {
+      let cb = cx.argument::<JsFunction>(0)?;
+      let this = cx.this();
+
+      let events = cx.borrow(&this, |emitter| Arc::clone(&emitter.events));
+      let emitter = EventEmitterTask(events);
+
+      emitter.schedule(cb);
+
+      Ok(JsUndefined::new().upcast())
+    }
+
+    method close(mut cx) {
+      let this = cx.this();
+
+      cx.borrow(&this, |emitter| emitter.shutdown.send(()))
+        .or_else(|err| cx.throw_error(&err.to_string()))?;
+      
+      Ok(JsUndefined::new().upcast())
     }
   }
 }
 
 register_module!(mut cx, {
-  cx.export_class::<JsConsumer>("Consumer")
+  cx.export_class::<JsConsumer>("Consumer")?;
+
+  Ok(())
 });
